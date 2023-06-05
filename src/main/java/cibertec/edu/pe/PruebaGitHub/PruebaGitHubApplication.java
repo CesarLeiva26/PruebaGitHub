9b972e6d28dad856deec8ceca9c464890243f35c
CesarLeiva26 <cesaremanuelleivajuro@gmail.com>
2023-06-05 16:11:13 -0500

    primer commit

---
+++ src/main/java/cibertec/edu/pe/PruebaGitHub/PruebaGitHubApplication.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.PruebaGitHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaGitHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaGitHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cibertec/edu/pe/PruebaGitHub/PruebaGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package cibertec.edu.pe.PruebaGitHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaGitHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
